6873e1b10f8214b1c71cebbbebcff214f3980551
Ayuse <ayodeleoluwasina@gmail.com>
2025-02-04 20:58:13 +0100

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="h-screen w-screen bg-[#212122] text-white flex items-center justify-center">
+        <!-- <NuxtRouteAnnouncer /> -->
+        <div class="text-xl">Raining Roses</div>
+        <div class="text-2xl">Coming Soon</div>
+    </div>
+</template>
